8193099265fa305cd33ee90b107e71fda33ba43c
arun <arunsadhasivam@yahoo.co.in>
2017-05-12 14:44:20 -0700

    Create employeeForm.py

---
+++ project/chAPI/model/employeeForm.py
@@ -0,0 +1,10 @@
+from unittest.util import _MAX_LENGTH
+from django.db import models
+ 
+class employee(models.Model):
+     
+    name = models.CharField(max_length=10)
+    empid = models.CharField(max_length=30)
+    employer = models.CharField(max_length=50)
+    
+          
