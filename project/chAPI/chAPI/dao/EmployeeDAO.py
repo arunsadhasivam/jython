fb4bec436fca98c81ab200f2c0f8cb0f20aaa423
arun <arunsadhasivam@yahoo.co.in>
2017-05-12 15:17:10 -0700

    Update EmployeeDAO.py

--- project/chAPI/chAPI/dao/EmployeeDAO.py
+++ project/chAPI/chAPI/dao/EmployeeDAO.py
@@ -6,40 +6,33 @@ from chAPI.model.employeeForm import employee as employee
 
 
 
+
+
 def search(request):
     print('dao')
     db = sqlite3.connect( database='C:\softwares\sqllite\db\emp.db')
     #db = sqlite3.connect( database='emp')
     cursor = db.cursor()
     employees=[];
-    cursor.execute('SELECT * FROM employee')
-    employees= cursor.fetchall();
-    print (employees);
-    
+    query = "SELECT * FROM employee"
+    cursor.execute('SELECT empid,name,employer FROM employee')
    
-    #employees = employee.from_db(db, name_map, employees);
-    print('----------')
-    print(employees[0][0])
-    
-   
-    #
-    #employee.objects.raw('SELECT * FROM employee',name_map)
-    #print ('employees1');
-    #print (employee.objects.values_list());
-    
-
-    
-   #names = ['arun','kumar','teja']
-    
-   #names = ['arun','kumar','teja']
-    #employees = [{'Name':'Arun','Empid':'101', 'employee':'wipro'}, 
-    #             {'Name':'Teja','Empid':'105', 'employee':'hcl'},
-    #             {'Name':'Test','Empid':'103', 'employee':'symantec'}]
-
-    #db.close() 
-   # employee.objects.raw(cursor.fetchall())
+    employees = dictfetchall(cursor)
+    db.close() 
+    # employee.objects.raw(cursor.fetchall())
     #for p in employee.objects.raw('SELECT id, first_name FROM myapp_person'):
-    #print(employee.employee.values_list());
+    #print(p);
         
 
     return render(request,  'home.html',{'employees': employees})
+
+
+
+
+def dictfetchall(cursor):
+    "Return all rows from a cursor as a dict"
+    columns = [col[0] for col in cursor.description]
+    return [
+    dict(zip(columns, row))
+    for row in cursor.fetchall()
+    ]
