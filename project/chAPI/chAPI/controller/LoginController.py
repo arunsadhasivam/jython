5d1dd6fd24c41c24754e91f8c177ce5e58e24970
arun <arunsadhasivam@yahoo.co.in>
2017-05-12 17:41:07 -0700

    Update LoginController.py

--- project/chAPI/chAPI/controller/LoginController.py
+++ project/chAPI/chAPI/controller/LoginController.py
@@ -3,10 +3,23 @@ from chAPI.service.APIService import APIService
 from chAPI.model.employeeForm import employee
 
 
+
+
+
 def  registration(request):
     return render(request,  'registration.html');
 
 def  search(request):
+    #if request.method == 'GET':
+    #    qd = request.GET
+    #elif request.method == 'POST':
+    # qd = request.POST */
+     
+    empId = request.GET['username']
+   
+    employee.empid =empId;
     employees =  APIService.searchEmployee('',employee);
     return render(request,  'home.html',{'employees': employees})
     
+
+
