4e1c7c557bf1adbfd0e8a72d4387df0fee5a3a71
arun <arunsadhasivam@yahoo.co.in>
2017-05-12 17:07:02 -0700

    Update employeeForm.py

--- project/chAPI/chAPI/model/employeeForm.py
+++ project/chAPI/chAPI/model/employeeForm.py
@@ -6,5 +6,9 @@ class employee(models.Model):
     name = models.CharField(max_length=10)
     empid = models.CharField(max_length=30)
     employer = models.CharField(max_length=50)
-    
+  
+   
+    def __str__(self):
+       return self.name;
+ 
           
